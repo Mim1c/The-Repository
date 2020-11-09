d2031fae6810020ed9f077919d01fdf84ad6d4f7
Michael Nielsen <58953067+Mim1c@users.noreply.github.com>
2020-11-09 14:40:11 +0100

    Hack

---
+++ ClientSideProgramming/script.js
@@ -0,0 +1,9 @@
+function NavBar() {
+    <div class="navBar">
+            <ul>
+                <li><a href="WebPage.html">Home</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
+        </div>
+}
